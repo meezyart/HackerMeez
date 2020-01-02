0b6f481b2dbcf8799aee687f1585048c7d14f2d7
Gamel James <gamel@meezyart.com>
2020-01-02 05:14:04 -0800

    Added Error handing to the API

--- src/services/hnAPI.js
+++ src/services/hnAPI.js
@@ -1,7 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/database";
 
-import { selectFields } from "../selectors/"
+import { selectFields } from "../selectors/";
 
 export const version = "/v0/";
 export const topStoriesUrl = `${version}topstories`;
@@ -14,32 +14,68 @@ firebase.initializeApp({
 
 export const getItemById = async itemId => {
   const itemRef = firebase.database().ref(itemUrl + itemId);
-  let results = await itemRef.once("value");
- return results.val();
-}
-
-export const getComment = async commentId => {
-  let results = await getItemById(commentId);
+  let results = "";
+  await itemRef.once(
+    "value",
+    snapshot => {
+      results = snapshot.val();
+    },
+    error => {
+      console.log("getItemById: We are getting this error:");
+      console.error(error);
+      results = error;
+    }
+  );
   return results;
 };
 
+export const getComment = async commentId => {
+  try {
+    let results = await getItemById(commentId);
+    return results;
+  } catch (error) {
+    console.log("getComment: We are getting this error:");
+    console.error(error);
+  }
+};
+
 export const getCommentIds = async storyId => {
-  let results = await getItemById(storyId);
-  return results.kids;
+  try {
+    let results = await getItemById(storyId);
+    return results.kids;
+  } catch (error) {
+    console.log("getCommentIds: We are getting this error:");
+    console.error(error);
+  }
 };
 
 export const getStory = async storyId => {
-  let results = await getItemById(storyId);
-  return selectFields(results);
+  try {
+    let results = await getItemById(storyId);
+    return selectFields(results);
+  } catch (error) {
+    console.log("getStory: We are getting this error:");
+    console.error(error);
+  }
 };
-
-export const getStoryIds = async (startCount = '0', endCount = '29') => {
+export const getStoryIds = async (startCount = "0", endCount = "29") => {
   const topStoriesRef = firebase
     .database()
     .ref(topStoriesUrl)
     .orderByKey()
     .startAt(startCount.toString())
     .endAt(endCount.toString());
-  let results = await topStoriesRef.once("value");
-  return results.val();
+  let results;
+  await topStoriesRef.once(
+    "value",
+    snapshot => {
+      results = snapshot.val();
+    },
+    error => {
+      console.log("getStoryIds: We are getting this error:");
+      console.error(error);
+      results = error;
+    }
+  );
+  return results;
 };
