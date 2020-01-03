0aabf49246f2b5edd68be7970d1d51e467cc395e
Gamel James <gamel@meezyart.com>
2020-01-02 20:11:34 -0800

    Moved Layout.js content to App.js
    Also made the 404 component a container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from "react";
-import { Layout } from "./containers/Layout.js"
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
-function App() {
-  return <Layout/>;
-}
+// components
+import { Header } from "./components/Header";
+// containers
+import { Error404 } from "./containers/Error404";
+import StoryList from "./containers/StoryList";
+import StoryDetail from "./containers/StoryDetail";
+// styles
+import { GlobalStyle, MainWrapper } from "./styles/GlobalStyle";
 
-export default App;
+export const App = () => {
+  return (
+    <MainWrapper>
+      <GlobalStyle />
+      <Header />
+      <section>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" render={() => <Redirect to="/new/0" />} />
+            <Route path="/new/:page" component={StoryList} />
+            <Route path="/item/:itemId" component={StoryDetail} />
+            <Route path="/404" component={Error404} />
+            <Route render={() => <Redirect to="/404" />} />
+          </Switch>
+        </BrowserRouter>
+      </section>
+    </MainWrapper>
+  );
+};
