fc5bf76dcf23ee570ed45eaa198a729119c0f3c9
Gamel James <gamel@meezyart.com>
2020-01-04 12:52:04 -0800

    App.js Updates
    Removed Comments
    updated Router
    Moved content wrapper to the root

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
-// components
 import { Header } from "./components/Header";
-// containers
 import { Error404 } from "./containers/Error404";
 import StoryList from "./containers/StoryList";
 import StoryDetail from "./containers/StoryDetail";
-// styles
-import { GlobalStyle, MainWrapper } from "./styles/GlobalStyle";
+
+import { GlobalStyle, MainWrapper, ContentWrapper } from "./styles/GlobalStyle";
 
 export const App = () => {
   return (
     <MainWrapper>
       <GlobalStyle />
       <Header />
-      <section>
+      <ContentWrapper>
         <BrowserRouter>
           <Switch>
             <Route exact path="/" render={() => <Redirect to="/new/0" />} />
-            <Route path="/new/:page" component={StoryList} />
+            <Route path="/new/:page(\d+)" component={StoryList} />
             <Route path="/item/:itemId" component={StoryDetail} />
-            <Route path="/404" component={Error404} />
-            <Route render={() => <Redirect to="/404" />} />
+            <Route component={Error404} />
           </Switch>
         </BrowserRouter>
-      </section>
+      </ContentWrapper>
     </MainWrapper>
   );
 };
