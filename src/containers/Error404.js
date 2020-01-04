baeeb0acbc8072ea19c64d0e4681570415c8ca87
Gamel James <gamel@meezyart.com>
2020-01-04 13:08:05 -0800

    Updated Error404 -adding a option to change error message
    Removed Content wrapper.

--- src/containers/Error404.js
+++ src/containers/Error404.js
@@ -1,16 +1,11 @@
-import React from 'react'
-import { ContentWrapper,ButtonLink } from '../styles/GlobalStyle'
+import React from "react";
+import { ButtonLink } from "../styles/GlobalStyle";
 
-export const Error404 = ({history}) => {
-  const handleRedirect = () =>{
-    history.push('/new/0')
-  }
+export const Error404 = ({message}) => {
   return (
-    <ContentWrapper>
-      <h1>Page not Found</h1>
-      <ButtonLink onClick={handleRedirect}>
-        Back to News
-      </ButtonLink>
-    </ContentWrapper>
-  )
-}
+    <>
+      <h1>{message || 'Page not Found'}</h1>
+      <ButtonLink to="/new/0">Back to News</ButtonLink>
+    </>
+  );
+};
